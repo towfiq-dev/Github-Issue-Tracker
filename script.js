642cde883da031028b652589b02c2825399d3120
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-03-07 15:43:55 +0600

    added Filtering (All, Open, Closed) and Search functionality (Search)

--- script.js
+++ script.js
@@ -61,22 +61,25 @@ function displayIssuesToUI(issuesArray) {
         issueCard.className = `card bg-white shadow-md hover:shadow-2xl transition-all border-t-4 ${borderColorClass} cursor-pointer p-5 h-full`;
 
         const cardContent = `
-            <div onclick="showIssueDetails('${singleIssue._id}')">
+            <div onclick="showIssueDetails('${singleIssue.id}')">
+                <div class="text-right mb-4">
+                <p class="text-[13px] font-semibold text-gray-600">${singleIssue.priority}</p>
+                </div>
                 <h2 class="text-md font-bold text-blue-700 leading-tight mb-2 truncate">${singleIssue.title}</h2>
                 <p class="text-xs text-gray-500 line-clamp-2 mb-4 h-8">${singleIssue.description}</p>
-                
                 <div class="flex flex-wrap gap-2 mb-4">
-                    <span class="px-2 py-1 bg-gray-100 text-[10px] rounded text-gray-600 font-medium">${singleIssue.category}</span>
-                    <span class="px-2 py-1 bg-yellow-50 text-[10px] rounded text-yellow-700 font-medium">${singleIssue.priority}</span>
+                    <span class="px-2 py-1 bg-gray-100 text-[10px] rounded text-gray-600 font-medium">${singleIssue.status}</span>
+                    <span class="px-2 py-1 bg-yellow-50 text-[10px] rounded text-yellow-700 font-medium">${singleIssue.lavels}</span>
+                    <span class="px-2 py-1 bg-yellow-50 text-[10px] rounded text-yellow-700 font-medium">${singleIssue.lavels}</span>
                 </div>
 
                 <div class="mt-4 pt-3 border-t flex justify-between items-center text-[10px] text-gray-400">
                     <div>
-                        <p class="font-semibold text-gray-600">${singleIssue.author}</p>
+                        <p class="font-semibold text-gray-600">#${singleIssue.id} by ${singleIssue.author}</p>
                         <p>${new Date(singleIssue.createdAt).toLocaleDateString()}</p>
                     </div>
                     <div class="text-right">
-                        <span class="font-bold uppercase">${singleIssue.label}</span>
+                        <p>Updated ${new Date(singleIssue.updatedAt).toLocaleDateString()}</p>
                     </div>
                 </div>
             </div>
@@ -89,4 +92,46 @@ function displayIssuesToUI(issuesArray) {
 
     
     document.getElementById('issue-count-text').innerText = `Total ${issuesArray.length} Issues Found`;
+}
+
+//Filtering (All, Open, Closed)
+function filterIssues(statusType) {
+
+    updateActiveButton(statusType);
+
+    if (statusType === 'all') {
+        displayIssuesToUI(allIssuesList);
+
+    } else {
+        const filteredData = allIssuesList.filter(function(item) {
+            const currentStatus = item.status.toLowerCase();
+            return currentStatus === statusType;
+        });
+
+        displayIssuesToUI(filteredData);
+    }
+}
+
+//Search functionality (Search)
+async function handleSearch() {
+    const searchText = document.getElementById('search-input').value;
+    
+    if (searchText === "") {
+        displayIssuesToUI(allIssuesList);
+        return;
+    }
+
+    toggleSpinner(true);
+    
+    try {
+        const searchUrl = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`;
+        const res = await fetch(searchUrl);
+        const searchData = await res.json();
+        
+        displayIssuesToUI(searchData.data);
+    } catch (e) {
+        console.log("Search error.");
+    } finally {
+        toggleSpinner(false);
+    }
 }
