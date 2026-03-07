9e693b9599e79350dc7267452584a3457af2f9a5
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-03-07 18:54:19 +0600

    added modal functionality, loading spinner and active button style

--- script.js
+++ script.js
@@ -60,24 +60,28 @@ function displayIssuesToUI(issuesArray) {
         
         issueCard.className = `card bg-white shadow-md hover:shadow-2xl transition-all border-t-4 ${borderColorClass} cursor-pointer p-5 h-full`;
 
-        const cardContent = `
+const cardContent = `
             <div onclick="showIssueDetails('${singleIssue.id}')">
-                <div class="text-right mb-4">
-                <p class="text-[13px] font-semibold text-gray-600">${singleIssue.priority}</p>
-                </div>
+            <div class="text-right mb-4">
+            <p class="text-[13px] font-semibold text-gray-600">${singleIssue.priority}</p>
+            </div>
                 <h2 class="text-md font-bold text-blue-700 leading-tight mb-2 truncate">${singleIssue.title}</h2>
                 <p class="text-xs text-gray-500 line-clamp-2 mb-4 h-8">${singleIssue.description}</p>
+                
                 <div class="flex flex-wrap gap-2 mb-4">
                     <span class="px-2 py-1 bg-gray-100 text-[10px] rounded text-gray-600 font-medium">${singleIssue.status}</span>
-                    <span class="px-2 py-1 bg-yellow-50 text-[10px] rounded text-yellow-700 font-medium">${singleIssue.lavels}</span>
+                    <span class="px-2 py-1 bg-gray-100 text-[10px] rounded text-gray-600 font-medium">${singleIssue.lavels}</span>
                     <span class="px-2 py-1 bg-yellow-50 text-[10px] rounded text-yellow-700 font-medium">${singleIssue.lavels}</span>
                 </div>
 
                 <div class="mt-4 pt-3 border-t flex justify-between items-center text-[10px] text-gray-400">
                     <div>
+                        
                         <p class="font-semibold text-gray-600">#${singleIssue.id} by ${singleIssue.author}</p>
+                        
                         <p>${new Date(singleIssue.createdAt).toLocaleDateString()}</p>
                     </div>
+                    
                     <div class="text-right">
                         <p>Updated ${new Date(singleIssue.updatedAt).toLocaleDateString()}</p>
                     </div>
@@ -134,4 +138,71 @@ async function handleSearch() {
     } finally {
         toggleSpinner(false);
     }
+}
+
+//Showing details via modal
+async function showIssueDetails(issueId) {
+    const detailUrl = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${issueId}`;
+    
+    const response = await fetch(detailUrl);
+    const result = await response.json();
+    const info = result.data;
+
+    const modalBox = document.getElementById('modal-body');
+
+    //Dynamically placing content inside a modal
+    modalBox.innerHTML = `
+        <div class="flex justify-between items-start mb-6">
+            <h2 class="text-2xl font-black text-gray-800">${info.title}</h2>
+            <button onclick="details_modal.close()" class="btn btn-sm btn-circle btn-ghost">✕</button>
+        </div>
+        
+        <div class="flex gap-1">
+        <p>${info.status}</p>
+        <p>* Opened by ${info.author}</p>
+        <p>* ${new Date(info.createdAt).toLocaleString()}</p>
+        </div>
+        <div class="flex gap-4">
+        <p>${info.lavels}</p>
+        <p>${info.lavels}</p>
+        </div>
+        <div class="space-y-4">
+            <p class="text-[#64748B] leading-relaxed bg-gray-50 p-4 rounded-xl italic">"${info.description}"</p>
+
+            <div class="bg-[#F8FAFC] grid grid-cols-1 md:grid-cols-2 gap-4 text-[16px] font-bold mt-6 p-3 border rounded-lg justify-between">
+                <div><span class="font-bold text-primary">Assignee:</span></br> ${info.author}</div>
+
+                <div><span class="font-bold text-primary">Priority:</span></br> ${info.priority}</div>
+
+            </div>
+        </div>
+
+        <div class="modal-action">
+            <button class="btn btn-primary text-white px-10" onclick="details_modal.close()">Close</button>
+        </div>
+    `;
+
+    details_modal.showModal();
+}
+
+//Toggle loading spinner
+function toggleSpinner(show) {
+    const spinner = document.getElementById('loading-spinner');
+    if (show === true) {
+        spinner.classList.remove('hidden');
+    } else {
+        spinner.classList.add('hidden');
+    }
+}
+
+//Changing the active button style
+function updateActiveButton(activeId) {
+
+    const buttons = document.querySelectorAll('.tab-btn');
+    buttons.forEach(function(btn) {
+        btn.classList.remove('btn-primary', 'text-white');
+    });
+
+    const targetBtn = document.getElementById(`btn-${activeId}`);
+    targetBtn.classList.add('btn-primary', 'text-white');
 }
